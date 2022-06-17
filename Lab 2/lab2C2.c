efad71d74acf45293a9a8e14ee8f66a4acc2e562
Araf A <araf.ahmed200@gmail.com>
2022-06-17 02:58:53 -0400

    Update lab2C2.c

--- Lab 2/lab2C2.c
+++ Lab 2/lab2C2.c
@@ -1,11 +1,3 @@
-/***************************************
- * 22S – Lab2 *
- * Author: Alam, Araf A *
- * Email: araf821@my.yorku.ca *
- * eecs_username: araf821 *
- * York Student #: 218124347
- ****************************************/
- 
 #include <stdio.h>
 
 #define SIZE 10
