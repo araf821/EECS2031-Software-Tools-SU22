2a2ca02b45001b0f0ee2ac97211c23de8c5c3b50
Araf A <araf.ahmed200@gmail.com>
2022-06-20 00:37:46 -0400

    Update lab4strcat.c

--- Lab 4/lab4strcat.c
+++ Lab 4/lab4strcat.c
@@ -1,11 +1,3 @@
-/***************************************
- * 22S - Lab4 *
- * Author: Alam, Araf *
- * Email: araf821@my.yorku.ca *
- * eecs_username: araf821 *
- * York Student #: 218124347
- ************************************** **/
-
 #include <stdio.h>
 #include <string.h>
 
