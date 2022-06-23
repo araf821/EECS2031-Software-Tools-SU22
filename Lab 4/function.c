f5c0104db964d1dbef9fd1714f67e4b3314e5b2c
Araf A <araf.ahmed200@gmail.com>
2022-06-23 04:16:10 -0400

    Update function.c

--- Lab 4/function.c
+++ Lab 4/function.c
@@ -1,10 +1,3 @@
-/***************************************
- * 22S - Lab4 *
- * Author: Alam, Araf A *
- * Email: araf821@my.yorku.ca *
- * eecs_username: araf821 *
- * York Student #: 218124347
- ************************************** **/
 #include <stdio.h>
 
 /* define global variables and a function */
